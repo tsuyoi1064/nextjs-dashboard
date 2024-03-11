120ac0bbbe6892b780bea49a6db2f5da688c8b63
tsuyoi1064 <tsuyoi1064@gmail.com>
2024-03-11 19:18:06 +0900

    update

--- app/api/route.ts
+++
@@ -1,7 +0,0 @@
-export async function GET() {
-    const url: string = `http://localhost:8080/movies`;
-
-    const res = await fetch(url)
-    const data = await res.json()
-    return Response.json({ data })
-}
